2fbd7a491b209a7c8e21c8b809b3c18275377cb9
Chao Ma <chao.ma@li.me>
2024-10-20 00:59:39 -0700

    init & cnn & resnet

---
+++ models/resnet.py
@@ -0,0 +1,49 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=False, stride=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels,
+                               kernel_size=kernel_size, padding=padding, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        # skip connection
+        self.skip = nn.Sequential()
+        if in_channels != out_channels:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels,
+                          kernel_size=1, bias=False, stride=1),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        output = F.relu(self.bn1(self.conv1(x)))
+        output = F.relu(self.bn2(self.conv2(output)))
+        output = F.relu(output + self.skip(x))
+        output = F.max_pool2d(output, 2, 2)
+        output = F.dropout(output, 0.2)
+        return output
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.block1 = ResNetBlock(3, 64) # input is (BatchSize, 3, 64, 64), output is (BatchSize, 64, 32, 32)
+        self.block2 = ResNetBlock(64, 128) # input is (BatchSize, 64, 32, 32), output is (BatchSize, 128, 16, 16)
+        self.block3 = ResNetBlock(128, 256) # input is (BatchSize, 128, 16, 16), output is (BatchSize, 256, 8, 8)
+        self.block4 = ResNetBlock(256, 512) # input is (BatchSize, 256, 8, 8), output is (BatchSize, 512, 4, 4)
+        self.fc = nn.Linear(512 * 4 * 4, num_classes)
+
+    def forward(self, x):
+        output = self.block1(x)
+        output = self.block2(output)
+        output = self.block3(output)
+        output = self.block4(output)
+        output = torch.flatten(output, 1)
+        output = self.fc(output)
+        return output
---
+++ train.py
@@ -0,0 +1,117 @@
+from datasets import load_dataset
+import torch
+from torch import nn, optim
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from models.resnet import ResNet
+
+# Load the dataset
+ds = load_dataset("uoft-cs/cifar10")
+
+
+# Define the transform function
+
+
+def transform():
+    return transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.Resize((72, 72)),
+        transforms.RandomCrop(64, padding=4),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010))
+    ])
+
+# Define the SimpleDataLoader class
+
+
+class SimpleDataLoader:
+    def __init__(self, ds, transform):
+        self.ds = ds
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, idx):
+        return self.transform(self.ds[idx]['img']), self.ds[idx]['label']
+
+
+# Create train and test datasets
+train_ds = SimpleDataLoader(ds["train"], transform())
+test_ds = SimpleDataLoader(ds["test"], transform())
+
+# Create data loaders
+train_loader = DataLoader(train_ds, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_ds, batch_size=64, shuffle=False)
+
+
+def train(epochs, logging_step, lr, batch_size, model):
+    # Initialize the model, loss function, and optimizer
+    device = torch.device(
+        "mps" if torch.backends.mps.is_available() else "cpu")
+    model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+    # Create data loaders
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+    # Training loop
+    for epoch in range(epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            if i % logging_step == 0:
+                print(f"Epoch :{epoch+1}, Step: {i+1}, Loss: {loss.item()}")
+
+        print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+
+        # Evaluation
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+            print(
+                f"Accuracy of the network on the 10000 test images: {100 * correct / total}%")
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+    sys.path.append(".")
+    from models.simple_cnn import SimpleCNN
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--logging_step", type=int, default=500)
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--batch_size", type=int, default=16)
+    args = parser.parse_args()
+
+    epochs = args.epochs
+    logging_step = args.logging_step
+    lr = args.lr
+    batch_size = args.batch_size
+    model = None
+    if args.model == "simple_cnn":
+        model = SimpleCNN()
+    elif args.model == "resnet":
+        model = ResNet()
+    else:
+        raise ValueError(f"Model {args.model} not found")
+    # model parameters
+    model_params = sum(p.numel() for p in model.parameters())
+    print(f"Model parameters: {model_params}")
+    train(epochs, logging_step, lr, batch_size, model)
